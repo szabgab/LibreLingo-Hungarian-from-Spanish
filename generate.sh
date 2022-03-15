2f5ac8f03d8908b117f5e10c921983a922d06aec
Gabor Szabo <gabor@szabgab.com>
2022-03-15 09:48:00 +0100

    start using new Two-way dictionary

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash -xe
 
-python ../LibreLingo-Judeo-Spanish-from-English/convert.py Spanish Hungarian
+#python ../LibreLingo-Judeo-Spanish-from-English/convert.py Spanish Hungarian
 ./LibreLingo/apps/tools/librelingo_tools/lili.py --export --html html/ --rel course
